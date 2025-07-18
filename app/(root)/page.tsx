6aec65ae0e692def1edf358922a8e4806238d0c5
Thomas Eby <tgeby@icloud.com>
2025-07-18 15:06:37 -0400

    Linked project with Shadcn, made global styles compatible with tailwindv4, added search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string }>
+}) {
+
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className='pink_container'>
+        <h1 className="heading">Pitch Your Startup, <br/> Connect With Entrepreneurs</h1>
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, Vote on Pitches, and Get Notices in Virtual Competitions.
+        </p>
+        <SearchForm query={query}/>
+      </section>
     </>
   );
 }
---
+++ components/SearchFormReset.tsx
@@ -0,0 +1,24 @@
+"use client"
+// The search form is server side, but the reset button must be client side
+import Link from "next/link";
+import { X } from "lucide-react";
+
+const SearchFormReset = () => {
+
+	const reset = () => {
+		const form = document.querySelector('.search-form') as HTMLFormElement;
+
+		if (form) form.reset();
+	};
+
+
+	return (
+		<button type='reset' onClick={reset}>
+			<Link href='/' className='search-btn text-white'>
+				<X className="size-5"></X>
+			</Link>
+		</button>
+	);
+};
+
+export default SearchFormReset;
